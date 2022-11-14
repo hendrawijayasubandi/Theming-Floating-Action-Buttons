9f9e89afa8791c31ceb3f3df5a759260f24dc9a8
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2022-11-14 14:06:08 +0700

    latihan custom button shape

---
+++ app/src/main/java/com/hendra/theming_floating_action_buttons/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hendra.theming_floating_action_buttons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
